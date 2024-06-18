5b015d1cc0ea0e86d9272479b24f1eded594f397
0VictorManuel0 <166439842+0VictorManuel0@users.noreply.github.com>
2024-06-18 13:37:36 -0600

    add: ejercicio 4

--- ejercicio_1/ejercicio4.js
+++ ejercicio_1/ejercicio4.js
@@ -1,1 +1,16 @@
-let
+let people = [
+    { name: "bob", id : 1 }, 
+    { name: "john", id : 2 },
+    { name: "alex", id : 3 }, 
+    { name: "john", id : 3 }
+];
+
+
+for (let i = 0; i < people.length; i++) {
+    const element = people[i];
+    console.log(element);
+    console.log(element.name);
+    if (element.name === ) {
+        
+    }
+}
